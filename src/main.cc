01ce2822d4a0716d9fbcf562ca4dcbcc5460e863
Alex Ameen <alex.ameen.tx@gmail.com>
2023-08-11 15:34:53 -0500

    command.cc

--- src/main.cc
+++ src/main.cc
@@ -31,6 +31,11 @@
 
 /* -------------------------------------------------------------------------- */
 
+struct VerboseParser;
+
+
+/* -------------------------------------------------------------------------- */
+
 struct PkgDbEnv : public flox::NixState {
   public:
 
@@ -50,132 +55,10 @@ struct PkgDbEnv : public flox::NixState {
     std::string                         dbPath;
     std::unique_ptr<flox::pkgdb::PkgDb> db;
     std::optional<flox::AttrPath>       attrPath;
-    std::optional<bool>                 force;
 
 
   private:
 
-      void
-    parseFlakeRefArg( const std::string & arg )
-    {
-      nix::FlakeRef ref =
-        ( arg.find( '{' ) == arg.npos ) ? nix::parseFlakeRef( arg )
-                                        : nix::FlakeRef::fromAttrs(
-                                            nix::fetchers::jsonToAttrs(
-                                              nlohmann::json::parse( arg )
-                                            )
-                                          );
-      {
-        nix::Activity act(
-          * nix::logger
-        , nix::lvlInfo
-        , nix::actUnknown
-        , nix::fmt( "fetching flake '%s'", ref.to_string() )
-        );
-        this->flake = std::make_optional(
-          flox::FloxFlake( (nix::ref<nix::EvalState>) this->state, ref )
-        );
-      }
-
-      if ( ! this->flake.value().lockedFlake
-                        .flake.lockedRef.input.hasAllInfo()
-         )
-        {
-          if ( nix::lvlWarn <= nix::verbosity )
-            {
-              nix::logger->warn(
-                "flake-reference is unlocked/dirty - "
-                "resulting DB may not be cached."
-              );
-            }
-        }
-    }
-
-
-  public:
-
-    /** Add verbosity flags to any parser and modify the global verbosity. */
-      static void
-    addVerbosity( argparse::ArgumentParser & parser )
-    {
-      /* Nix verbosity levels for reference:
-       *   typedef enum {
-       *     lvlError = 0   ( --quiet --quiet --quiet )
-       *   , lvlWarn        ( --quiet --quiet )
-       *   , lvlNotice      ( --quiet )
-       *   , lvlInfo        ( **Default** )
-       *   , lvlTalkative   ( -v )
-       *   , lvlChatty      ( -vv   | --debug --quiet )
-       *   , lvlDebug       ( -vvv  | --debug )
-       *   , lvlVomit       ( -vvvv | --debug -v )
-       *   } Verbosity;
-       */
-      parser.add_argument( "-q", "--quiet" )
-        .help(
-          "Decreate the logging verbosity level. May be used up to 3 times."
-        ).action(
-          [&]( const auto & )
-          {
-            nix::verbosity =
-              ( nix::verbosity <= nix::lvlError )
-                ? nix::lvlError : (nix::Verbosity) ( nix::verbosity - 1 );
-          }
-        ).default_value( false ).implicit_value( true )
-        .append()
-      ;
-      parser.add_argument( "-v", "--verbose" )
-        .help( "Increase the logging verbosity level. May be up to 4 times." )
-        .action(
-          [&]( const auto & )
-          {
-            nix::verbosity =
-              ( nix::lvlVomit <= nix::verbosity )
-                ? nix::lvlVomit : (nix::Verbosity) ( nix::verbosity + 1 );
-          }
-        ).default_value( false ).implicit_value( true )
-        .append()
-      ;
-    }
-
-
-    /**
-     * Add `target` argument to any parser to read either a `flake-ref` or
-     * path to an existing database.
-     */
-      void
-    addTarget( argparse::ArgumentParser & parser )
-    {
-      parser.add_argument( "target" )
-        .help(
-          "The source (database path or flake-ref) to read"
-        )
-        .required()
-        .metavar( "DB-OR-FLAKE-REF" )
-        .action(
-          [&]( const std::string & target )
-          {
-            if ( isSQLiteDb( target ) )
-              {
-                this->flake  = std::nullopt;
-                this->dbPath = nix::absPath( target );
-                this->db     =
-                  std::make_unique<flox::pkgdb::PkgDb>( this->dbPath );
-              }
-            else  /* flake-ref */
-              {
-                this->parseFlakeRefArg( target );
-
-                this->dbPath =
-                  flox::pkgdb::genPkgDbName( this->flake.value().lockedFlake );
-
-                this->db = std::make_unique<flox::pkgdb::PkgDb>(
-                  this->flake.value().lockedFlake, this->dbPath
-                );
-              }
-          }
-        )
-      ;
-    }
 
 
     PkgDbEnv( int argc, char * argv[] )
@@ -189,63 +72,12 @@ struct PkgDbEnv : public flox::NixState {
       this->pScrape.add_description( "Scrape a flake and emit a SQLite3 DB" );
       PkgDbEnv::addVerbosity( this->pScrape );
 
-      this->pScrape.add_argument( "-d", "--database" )
-        .help( "Use database at PATH" )
-        .default_value( "" )
-        .metavar( "PATH" )
-        .nargs( 1 )
-        .action( [&]( const std::string & dbPath )
-                 {
-                   this->dbPath = nix::absPath( dbPath );
-                 }
-               )
-      ;
-
       this->pScrape.add_argument( "-f", "--force" )
         .help( "Force re-evaluation of prefix" )
         .default_value( false )
         .implicit_value( true )
         .nargs( 0 )
         .action( [&]( const bool & v ) { this->force = v; } )
-      ;
-
-      this->pScrape.add_argument( "flake-ref" )
-        .help( "A flake-reference URI string ( preferably locked )" )
-        .required()
-        .metavar( "FLAKE-REF" )
-        .action( [&]( const std::string & target )
-                 {
-                   this->parseFlakeRefArg( target );
-                 }
-               )
-      ;
-
-      /* Sets the attribute path to be scraped.
-       * If no system is given use the current system.
-       * If we're searching a catalog and no stability is given, use "stable".
-       */
-      this->pScrape.add_argument( "attr-path" )
-        .help( "Attribute path to scrape" )
-        .metavar( "ATTR" )
-        .remaining()
-        .default_value( flox::AttrPath {
-          "packages"
-        , nix::settings.thisSystem.get()
-        } )
-        .action( [&]( const flox::AttrPath & given )
-                 {
-                   flox::AttrPath path = given;
-                   if ( path.size() < 2 )
-                     {
-                       path.push_back( nix::settings.thisSystem.get() );
-                     }
-                   if ( ( path.size() < 3 ) && ( path[0] == "catalog" ) )
-                     {
-                       path.push_back( "stable" );
-                     }
-                   this->attrPath = std::make_optional( path );
-                 }
-               )
       ;
 
       /* Add the scrape subcommand. */
@@ -257,93 +89,13 @@ struct PkgDbEnv : public flox::NixState {
 
 /* -------------------------------------------------------------------------- */
 
-  static int
-scrapeImplementation( argparse::ArgumentParser & cmdScrape
-                    , nix::Verbosity           & verbosity
-                    )
-{
-
-  /* Initialize `nix' */
-
-  // TODO: Handle `--store'
-  flox::NixState nstate( verbosity );
-
-
-  /* Fetch and lock our flake. */
-
-
-  /* Open our database. */
-
-  /* Maybe use path from arguments. */
-  std::string dbPathStr;
-  if ( cmdScrape.is_used( "-d" ) )
-    {
-      dbPathStr = cmdScrape.get<std::string>( "-d" );
-      if ( ! dbPathStr.empty() ) { dbPathStr = nix::absPath( dbPathStr ); }
-    }
-
-  /* Fallback to generated path under `XDG_CACHE_DIR/flox/pkgdb-v0/' */
-  if ( dbPathStr.empty() )
-    {
-      dbPathStr = flox::pkgdb::genPkgDbName( flake.lockedFlake );
-    }
-
-  /* Create directories if needed. */
-  std::filesystem::path dbPath( dbPathStr );
-  if ( ! std::filesystem::exists( dbPath.parent_path() ) )
-    {
-      std::filesystem::create_directories( dbPath.parent_path() );
-    }
-
-  /* Open/Create database. */
-  flox::pkgdb::PkgDb db( flake.lockedFlake, dbPathStr );
-
-
-  /* If we haven't processed this prefix before or `--force' was given, open the
-   * eval cache and start scraping. */
-
-  if ( cmdScrape.get<bool>( "-f" ) || ( ! db.hasPackageSet( attrPath ) ) )
-    {
-      std::vector<nix::Symbol> symbolPath;
-      for ( const auto & a : attrPath )
-        {
-          symbolPath.emplace_back( flake.state->symbols.create( a ) );
-        }
-
-      flox::pkgdb::Todos todo;
-      if ( flox::MaybeCursor root = flake.maybeOpenCursor( symbolPath );
-           root != nullptr
-         )
-        {
-          todo.push(
-            std::make_pair( std::move( attrPath ), (flox::Cursor) root )
-          );
-        }
-
-      while ( ! todo.empty() )
-        {
-          auto & [prefix, cursor] = todo.front();
-          scrape( db, flake.state->symbols, prefix, cursor, todo );
-          todo.pop();
-        }
-    }
-
-
-  /* Print path to database. */
-  std::cout << dbPathStr << std::endl;
-
-  return EXIT_SUCCESS;  /* GG, GG */
-}
-
-
-/* -------------------------------------------------------------------------- */
-
   int
 main( int argc, char * argv[] )
 {
 
   /* Define arg parsers. */
 
+  #if 0
   argparse::ArgumentParser cmdGet( "get" );
   cmdGet.add_description( "Query package attributes" );
 
@@ -371,7 +123,8 @@ main( int argc, char * argv[] )
 
 
   prog.add_subparser( cmdGet );
-  
+
+
 
 /* -------------------------------------------------------------------------- */
 
@@ -392,6 +145,7 @@ main( int argc, char * argv[] )
     {
       return scrapeImplementation( cmdScrape, verbosity );
     }
+  #endif
 
   return EXIT_FAILURE;
 
