ddd15947a747ce710b6dd697d14db46b5448049e
Alex Ameen <alex.ameen.tx@gmail.com>
2023-08-23 17:29:16 -0500

    doc: isSQLiteError

--- include/flox/pkgdb/read.hh
+++ include/flox/pkgdb/read.hh
@@ -284,6 +284,11 @@ enum match_strength {
 
 /* -------------------------------------------------------------------------- */
 
+/**
+ * Predicate to detect failing SQLite3 return codes.
+ * @param rc A SQLite3 _return code_.
+ * @return `true` iff @a rc is a SQLite3 error.
+ */
 bool isSQLError( int rc );
 
 
