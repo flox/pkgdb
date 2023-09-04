ce3288565036f5c0f79c5a183d3ae9dda7dc1719
Alex Ameen <alex.ameen.tx@gmail.com>
2023-09-04 13:29:54 -0500

    doc

--- include/compat/concepts.hh
+++ include/compat/concepts.hh
@@ -23,12 +23,21 @@ namespace std {
 
 /* -------------------------------------------------------------------------- */
 
+/**
+ * The concept `destructible` specifies the concept of all types whose
+ * instances can safely be destroyed at the end of their lifetime
+ * ( including reference types ).
+ */
   template <class T>
 concept destructible = std::is_nothrow_destructible_v<T>;
 
 
 /* -------------------------------------------------------------------------- */
 
+/**
+ * The `constructible_from` concept specifies that a variable of type @a T can
+ * be initialized with the given set of argument types @a Args....
+ */
   template <class T, class... Args>
 concept constructible_from =
   std::destructible<T> && std::is_constructible_v<T, Args...>;
@@ -36,6 +45,12 @@ concept constructible_from =
 
 /* -------------------------------------------------------------------------- */
 
+/**
+ * The concept `convertible_to<From, To>` specifies that an expression of the
+ * same type and value category as those of `std::declval<From>()` can be
+ * implicitly and explicitly converted to the type @a To, and the two forms of
+ * conversion are equivalent.
+ */
   template <class From, class To>
 concept convertible_to =
   std::is_convertible_v<From, To> &&
