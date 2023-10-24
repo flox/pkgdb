ddd1733b1d620d2bf8d1b2b8153584fef6187d86
Alex Ameen <alex.ameen.tx@gmail.com>
2023-10-24 10:48:57 -0500

    manifest must be an object

--- src/resolver/manifest.cc
+++ src/resolver/manifest.cc
@@ -231,6 +231,14 @@ fixupDescriptor( const std::string & iid, ManifestDescriptor & desc )
 void
 from_json( const nlohmann::json & jfrom, ManifestRaw & manifest )
 {
+  if ( ! jfrom.is_object() )
+    {
+      std::string aOrAn = jfrom.is_array() ? " an " : " a ";
+      throw InvalidManifestFileException(
+        "Manifest must be an object, but is" + aOrAn
+        + std::string( jfrom.type_name() ) + '.' );
+    }
+
   for ( const auto & [key, value] : jfrom.items() )
     {
       if ( key == "install" )
